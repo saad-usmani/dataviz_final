92c3f92fe33ce617764b3a608bbd8917432ba34f
saad-usmani <saadu.usmani@gmail.com>
2018-05-09 01:19:09 -0400

    summary isnt quite working

--- rough script.R
+++ rough script.R
@@ -66,4 +66,53 @@ p2<- d %>%
     x = 1, xanchor = "right", y = 0, yanchor = "bottom", color = 'white'
   )
 
+dis_sum <- dis %>%
+  group_by(disease, year) %>%
+  summarise(total = sum(count)) %>%
+  filter(disease == 'Measles') %>%
+  data.frame()
 
+
+d3<-dis_sum%>%
+  accumulate_by(~year)
+  
+
+p3<- d3 %>%
+  plot_ly(
+    x = ~year, 
+    y = ~total,
+    frame = ~frame, 
+    text = ~paste('Count: ', total), 
+    color = 'orange',
+    hoverinfo = "text",
+    type = 'scatter',
+    mode = 'lines'
+  ) %>%
+  layout(
+    title = paste(unique(dis_sum$disease), 'overall count over time'),
+    font = list(color = 'white', size = 12, font = ''),
+    xaxis = list(
+      type = "-",
+      title = 'Year',
+      color = 'white'
+    ),
+    yaxis = list(
+      type = "-",
+      title = 'Count',
+      color = 'white'
+    ),
+    paper_bgcolor = 'black',
+    plot_bgcolor = 'black'
+  )%>%
+  animation_opts(
+    easing = 'elastic',
+    frame = 75, 
+    transition = 50, 
+    redraw = FALSE
+  ) %>%
+  animation_slider(
+    hide = T
+  ) %>%
+  animation_button(
+    x = 1, xanchor = "right", y = 0, yanchor = "bottom", color = 'white'
+  )
